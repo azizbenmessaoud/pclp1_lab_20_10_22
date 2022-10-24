6efe670e9806c023baa6907136c79224096f4cbd
[azizbenmessaoud] <[bnmessaoudaziz@gmail.com]>
2022-10-25 01:23:12 +0300

    program 3 added

---
+++ program3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    for (int i=1; i <=100; i++) {
+        if (i%3 == 0 && i%5 == 0) printf("fizz buzz ");
+        else if (i%3 == 0) printf("fizz ");
+        else if(i%5 == 0) printf("buzz ");
+        else printf("%d ", i);
+    }
+    return 0;
+}
